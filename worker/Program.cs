30d9d1b1b422533d86af6bca38df933ab5566a68
Akbar <muhammadzafirulakbar88@gmail.com>
2025-06-25 10:13:22 +0000

    Update worker to connect PostgreSQL

--- worker/Program.cs
+++ worker/Program.cs
@@ -46,7 +46,7 @@ namespace Worker
                         if (!pgsql.State.Equals(System.Data.ConnectionState.Open))
                         {
                             Console.WriteLine("Reconnecting DB");
-                            f41c123 (Update worker to connect PostgreSQl)
+                            
                             pgsql = OpenDbConnection("Host=db;Port=5432;Username=postgres;Password=postgres;");
                         }
                         else
